83a83062bc8623ae452c18e4f3459da693ef4002
Leo Gordon <lg4@krai.ai>
2023-01-18 16:31:20 +0000

    CPU accuracy seems to be the same across platforms

--- test_axs2mlperf.sh
+++ test_axs2mlperf.sh
@@ -3,10 +3,10 @@
 source assert.sh
 
 if [ "$ONNX_DETECTION" == "on" ]; then
-    axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100
-    ACCURACY_OUTPUT=`axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100 , get accuracy`
+    axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu
+    ACCURACY_OUTPUT=`axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu , get accuracy`
     echo "Accuracy: $ACCURACY_OUTPUT"
-    assert 'echo $ACCURACY_OUTPUT' '22.963'
-    axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100 --- , remove
+    assert 'echo $ACCURACY_OUTPUT' '23.015'
+    axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu --- , remove
     assert_end object_detection_onnx_loadgen_py
 fi
