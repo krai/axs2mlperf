033fc24e6bfdd3a6604de543bdf8ef50c6fac6ea
Leo Gordon <lg4@krai.ai>
2023-01-18 18:32:41 +0000

    testfix: rounding the accuracy for comparison (they seem to differ depending on CPU/GPU and Python version choice)

--- test_axs2mlperf.sh
+++ test_axs2mlperf.sh
@@ -4,9 +4,9 @@ source assert.sh
 
 if [ "$ONNX_DETECTION" == "on" ]; then
     axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu
-    ACCURACY_OUTPUT=`axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu , get accuracy`
-    echo "Accuracy: $ACCURACY_OUTPUT"
-    assert 'echo $ACCURACY_OUTPUT' '23.015'
+    ACCURACY_OUTPUT=`axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu , get accuracy ,0 func float ,0 func round 1`
+    echo "Rounded accuracy: $ACCURACY_OUTPUT"
+    assert 'echo $ACCURACY_OUTPUT' '23.0'
     axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu --- , remove
     assert_end object_detection_onnx_loadgen_py
 fi
