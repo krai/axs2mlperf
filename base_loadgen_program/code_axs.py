fbf1db5129630d3b5c9763b36cac755be6184a5d
Maria Mirkina <maria@krai.ai>
2023-02-28 11:22:34 +0000

    Added dictionary of internal and mlperf model name in base_loadgen_program for audit.conf

--- base_loadgen_program/code_axs.py
+++ base_loadgen_program/code_axs.py
@@ -1,7 +1,14 @@
 import os
 from shutil import copy2
 
-def generate_user_conf(loadgen_param_dictionary, model_name, loadgen_scenario, target_user_conf_path, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config):
+def get_mlperf_model_name(model_name_compliance_dict, model_name):
+    if model_name in model_name_compliance_dict.keys():
+        print("DEBUG: model_name_dict[model_name] = ", model_name_compliance_dict[model_name])
+        return model_name_compliance_dict[model_name]
+    else:
+        return None
+
+def generate_user_conf(loadgen_param_dictionary, model_name, loadgen_scenario, target_user_conf_path, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config, model_name_compliance_dict):
     param_to_conf_pair = {
         "loadgen_count_override_min":   ("min_query_count", 1),
         "loadgen_count_override_max":   ("max_query_count", 1),
@@ -29,13 +36,16 @@ def generate_user_conf(loadgen_param_dictionary, model_name, loadgen_scenario, t
          user_conf_file.writelines(user_conf)
 
     if loadgen_compliance_test:
-        target_audit_conf_path = generate_audit_conf( model_name, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config)
+        target_audit_conf_path = generate_audit_conf( model_name, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config, model_name_compliance_dict)
 
     return target_user_conf_path
 
-def generate_audit_conf( model_name, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config):
+def generate_audit_conf( model_name, loadgen_mlperf_path, target_audit_conf_path, loadgen_compliance_test, compliance_test_config, model_name_compliance_dict):
 
     # Copy 'audit.config' for compliance testing into the current directory.
+    mlperf_model_name = get_mlperf_model_name(model_name_compliance_dict, model_name)
+    if mlperf_model_name is not None:
+        model_name = mlperf_model_name
 
     path_parts = [ loadgen_mlperf_path, 'compliance', 'nvidia', loadgen_compliance_test ]
 
