b4f071b5b9849ebe1c8053028f4fd76958baad04
Leo Gordon <lg4@krai.ai>
2023-04-27 17:22:25 +0100

    testfix: rounding to whole numbers now

--- test_axs2mlperf.sh
+++ test_axs2mlperf.sh
@@ -6,7 +6,7 @@ if [ "$ONNX_DETECTION" == "on" ]; then
     axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu
     ACCURACY_OUTPUT=`axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu , get accuracy`
     echo "Accuracy: $ACCURACY_OUTPUT"
-    assert 'echo `axs func round $ACCURACY_OUTPUT 1`' '23.0'
+    assert 'echo `axs func round $ACCURACY_OUTPUT 0`' '23.0'
     axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_scenario=Offline,loadgen_mode=AccuracyOnly,model_name=ssd_resnet34,loadgen_dataset_size=20,loadgen_buffer_size=100,execution_device=cpu --- , remove
     assert_end object_detection_onnx_loadgen_py
 
