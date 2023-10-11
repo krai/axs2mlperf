a08c3746b81dd4a009a8088efc415bc1c99aebc1
Elim Kwan <elim@krai.ai>
2023-10-11 11:46:36 +0100

    Add automated tests. Trigger pull request to stable when master is updated and passed the tests. (#20)
    * Add github action.
    
    * Change to self-hosted server.
    
    * Update path for test.
    
    * Update docker command for non-iterative env.
    
    * Add permission for docker.
    
    * fix docker
    
    * Fix flag.
    
    * Persist docker container.
    
    * Fix docker run.
    
    * Update run_test.sh
    
    * Run container at the background
    
    * Update run_test.sh
    
    * Update run_test.sh
    
    * hardcode run_docker
    
    * Update run_test.sh
    
    * Update run_test.sh
    
    * Update run_test.sh
    
    * Update run_test.sh
    
    * Update run_test.sh
    
    * Update docker-image.yml
    
    * Clean up run_test.sh
    
    * Update docker-image.yml
    
    ---------
    
    Co-authored-by: xihajun <junfan@krai.ai>

--- demo/run_test.sh
+++ demo/run_test.sh
@@ -6,7 +6,7 @@ source assert.sh
 _IMAGE_NAME=${IMAGE_NAME:-axs:benchmarks.test}
 
 run_docker () {
-    docker run -it --rm ${_IMAGE_NAME} -c "$1"
+    docker run --rm --privileged ${_IMAGE_NAME} -c "$1"
 }
 
 cmd=$(run_docker "axs byquery loadgen_output,task=object_detection,framework=onnxrt,loadgen_dataset_size=20,model_name=retinanet_openimages , get accuracy")
@@ -15,13 +15,11 @@ num=${cmd:0:2}
 assert "echo ${num}" "52"
 assert_end object_detection_retinanet_openimages_benchmark
 
-
 cmd=$(run_docker "axs byquery loadgen_output,task=object_detection,framework=onnxrt,loadgen_dataset_size=20,model_name=retinanet_coco , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
 num=${cmd:0:2}
 assert "echo ${num}" "34"
 assert_end object_detection_retinanet_coco_benchmark
-
 
 cmd=$(run_docker "axs byquery loadgen_output,task=image_classification,framework=onnxrt,loadgen_dataset_size=20 , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
