92d571d6aff627d5882e126ad37744886f8d9e43
elim <elim@krai.ai>
2023-10-11 10:58:18 +0100

    Persist docker container.

--- demo/run_test.sh
+++ demo/run_test.sh
@@ -7,14 +7,21 @@ _IMAGE_NAME=${IMAGE_NAME:-axs:benchmarks.test}
 
 IS_SERVER=${IS_SERVER:-false}
 
+
+
 run_docker () {
     if [[ ${IS_SERVER} = true ]]
     then 
-	    docker run -dt --rm ${_IMAGE_NAME} -c "$1"
+	    docker exec git_bot "$1"
     else
             docker run -it --rm ${_IMAGE_NAME} -c "$1"
     fi
 }
+
+if [[ ${IS_SERVER} = true ]]
+then
+	docker run --name git_bot --privileged -t -d -f /dev/null
+fi
 
 cmd=$(run_docker "axs byquery loadgen_output,task=object_detection,framework=onnxrt,loadgen_dataset_size=20,model_name=retinanet_openimages , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
@@ -43,3 +50,9 @@ assert "echo ${num}" "85"
 assert_end bert_benchmark
 
 rm assert.sh
+
+
+if [[ ${IS_SERVER} = true ]]
+then
+	docker stop git_bot && docker rm git_bot
+fi
