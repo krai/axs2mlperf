5ff3d6f33f3ac0f51c83c1c76209ff73d4f9c400
Elim Kwan <elim@krai.ai>
2023-10-11 11:41:59 +0100

    Clean up run_test.sh

--- demo/run_test.sh
+++ demo/run_test.sh
@@ -5,25 +5,9 @@ source assert.sh
 
 _IMAGE_NAME=${IMAGE_NAME:-axs:benchmarks.test}
 
-# IS_SERVER=${IS_SERVER:-false}
-
-# (docker stop git_bot && docker rm git_bot) || true
-
-# docker run --name git_bot --entrypoint tail --privileged -t ${_IMAGE_NAME} -f /dev/null &
-
-# run_docker () {
-#     if [[ ${IS_SERVER} = true ]]
-#     then 
-# 	    docker exec git_bot $1
-#     else
-#             docker run -it --rm ${_IMAGE_NAME} -c "$1"
-#     fi
-# }
-
 run_docker () {
     docker run --rm --privileged ${_IMAGE_NAME} -c "$1"
 }
-
 
 cmd=$(run_docker "axs byquery loadgen_output,task=object_detection,framework=onnxrt,loadgen_dataset_size=20,model_name=retinanet_openimages , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
@@ -31,13 +15,11 @@ num=${cmd:0:2}
 assert "echo ${num}" "52"
 assert_end object_detection_retinanet_openimages_benchmark
 
-
 cmd=$(run_docker "axs byquery loadgen_output,task=object_detection,framework=onnxrt,loadgen_dataset_size=20,model_name=retinanet_coco , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
 num=${cmd:0:2}
 assert "echo ${num}" "34"
 assert_end object_detection_retinanet_coco_benchmark
-
 
 cmd=$(run_docker "axs byquery loadgen_output,task=image_classification,framework=onnxrt,loadgen_dataset_size=20 , get accuracy")
 echo "Accuracy(20 samples): ${cmd}"
@@ -52,6 +34,3 @@ assert "echo ${num}" "85"
 assert_end bert_benchmark
 
 rm assert.sh
-
-# docker stop git_bot && docker rm git_bot
-
