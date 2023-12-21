8faa115c2f735d4cc7ccb06eb17431ca1622df20
saheli@krai.ai <saheli@krai.ai>
2023-12-21 18:06:21 +0000

    Fix typo accurcay -> accuracy.

--- submitter/code_axs.py
+++ submitter/code_axs.py
@@ -560,7 +560,7 @@ def generate_tables(experiment_entries, division, submitter, submission_entry, _
         # Target accuracy for workloads
         image_classification_target_accuracy = round(76.46*0.99, 3)
         bert-99_target_accuracy = round(90.874 * 0.99 , 3)
-        bert-99.9_target_accurcay = round(90.874 * 0.999, 3)
+        bert-99.9_target_accuracy = round(90.874 * 0.999, 3)
         object_dectection_target_accuracy = round(37.55*0.99 , 3)
 
         if mode.lower() == "performance":
