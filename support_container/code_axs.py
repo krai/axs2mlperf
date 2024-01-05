1a9a251cd7fcd9a63c6fd83a04e118c456a6f607
Maria Mirkina <maria@krai.ai>
2024-01-05 04:41:37 -0600

    Removed unused parameters in support_container

--- support_container/code_axs.py
+++ support_container/code_axs.py
@@ -7,6 +7,5 @@ def create_mounted_string(local_dir, container_local_dir, repo_list, local_exper
         else:
             end = ""
         result += " -v " + local_dir + elem + ":" + container_local_dir + elem + end
-    #result += " -v " + local_dir + local_experiment_dir_name + ":" + container_local_dir + local_experiment_dir_name
     return result
     
