153699d896cf94add8e809f4b123b1106095d660
Maria Mirkina <maria@krai.ai>
2024-01-08 05:33:50 -0600

    Renamed some parameters in docker_qaic

--- docker_qaic/code_axs.py
+++ docker_qaic/code_axs.py
@@ -8,4 +8,19 @@ def create_mounted_string(local_dir, container_local_dir, repo_list, local_exper
             end = ""
         result += " -v " + local_dir + elem + ":" + container_local_dir + elem + end
     return result
-    
+
+def get_image_name(task):
+    if task == "image_classification":
+        image_name_part = "resnet50.full"
+    elif task == "object_detection":
+        image_name_part = "retinanet"
+    elif task == "bert":
+        image_name_part = "bert"
+    return "krai/mlperf." + image_name_part
+
+def get_model_name_container(model_name):
+    if model_name in [ "bert-99", "bert-99.9" ]:
+       result = "bert"
+    else:
+        result = model_name
+    return result
