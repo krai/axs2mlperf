3efdfe017e4522dfa9ae61253f52bb2e5da7dd65
Oleg Mosalov <oleg@krai.ai>
2025-02-19 12:10:51 +0100

    Added a base recipe for Llama3.1-405b experiments. Added a recipe for the dataset for Llama3.1-405b.

---
+++ base_llama3_1_loadgen_experiment/code_axs.py
@@ -0,0 +1,55 @@
+import json
+
+from transformers import AutoTokenizer
+
+def get_accuracy_dict(accuracy_dict_full):
+    accuracy_dict = {}
+    for k in accuracy_dict_full.keys():
+        if k in ["rougeL", "exact_match", "tokens_per_sample"]:
+            accuracy_dict[k] = accuracy_dict_full[k]
+    return accuracy_dict
+
+def parse_tokens(
+    tokenised_accuracy_log_path: str, output_log_path: str
+):
+    with open(tokenised_accuracy_log_path) as f:
+        log = json.load(f)
+
+    output_log = []
+    for item in log:
+        hex_str = item["data"]
+        hex_tokens = [hex_str[i : i + 8] for i in range(0, len(hex_str), 8)]
+        tokens = [
+            int.from_bytes(bytes.fromhex(tok), byteorder="little") for tok in hex_tokens
+        ]
+        output_log.append(tokens)
+
+    with open(output_log_path, "w") as f:
+        json.dump(output_log, f, indent=2)
+    return output_log_path
+
+def detokenise(
+    checkpoint_path: str, tokenised_accuracy_log_path: str, output_log_path: str
+):
+    tokeniser = AutoTokenizer.from_pretrained(checkpoint_path)
+
+    with open(tokenised_accuracy_log_path) as f:
+        log = json.load(f)
+
+    output_log = []
+    for item in log:
+        hex_str = item["data"]
+        hex_tokens = [hex_str[i : i + 8] for i in range(0, len(hex_str), 8)]
+        tokens = [
+            int.from_bytes(bytes.fromhex(tok), byteorder="little") for tok in hex_tokens
+        ]
+        output_log.append({
+            "seq_id" : item["seq_id"],
+            "qsl_idx" : item["qsl_idx"],
+            "data": tokeniser.decode(tokens),
+            "token_count" : item["token_count"]
+        })
+
+    with open(output_log_path, "w") as f:
+        json.dump(output_log, f, indent=2)
+    return output_log_path
