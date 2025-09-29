da92521cf93f3eff3d8123c8efd931c4ff8bbc89
Oleg Mosalov <oleg@krai.ai>
2025-09-29 17:31:34 +0200

    Added a way to call get dtypes_used on a saved safetensors model.

---
+++ base_safetensors_model/code_axs.py
@@ -0,0 +1,20 @@
+from safetensors import safe_open
+import math
+from collections import defaultdict
+from pathlib import Path
+
+def get_dtypes_used(__entry__=None):
+    files = list(Path(__entry__.get_path()).glob("*.safetensors"))
+
+    param_counts = defaultdict(int)
+
+    for fname in files:
+        with safe_open(fname, framework="pt") as f:
+            for key in f.keys():
+                tensor = f.get_tensor(key)
+                dtype = str(tensor.dtype)
+                shape = tensor.shape
+                num_params = math.prod(shape)
+                param_counts[dtype] += num_params
+
+    return dict(param_counts)
