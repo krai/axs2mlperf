8d10c21437b87a5840554377fb5399a23fe17522
Oleg Mosalov <oleg@krai.ai>
2026-01-21 15:10:37 +0100

    Better parsing.

--- text_to_video_reference_loadgen/code_axs.py
+++ text_to_video_reference_loadgen/code_axs.py
@@ -1,6 +1,8 @@
 def get_volumes(volumes_mapping, output_entry=None):
     volume_strings = []
-    for host_path_key, container_path_key in volumes_mapping:
+    for i in range(0, len(volumes_mapping), 2):
+        host_path_key = volumes_mapping[i]
+        container_path_key = volumes_mapping[i+1]
         host_path = output_entry.get(host_path_key)
         container_path = output_entry.get(container_path_key)
         volume_strings.append(f"-v {host_path}:{container_path}")
