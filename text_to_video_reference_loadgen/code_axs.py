6fe2023ec2a5ad54b4e84a95f90bd022eeb3df31
Oleg Mosalov <oleg@krai.ai>
2026-01-21 15:14:22 +0100

    Another attempt to make the mapping to work.

--- text_to_video_reference_loadgen/code_axs.py
+++ text_to_video_reference_loadgen/code_axs.py
@@ -1,9 +1,7 @@
 def get_volumes(volumes_mapping, output_entry=None):
     volume_strings = []
     for i in range(0, len(volumes_mapping), 2):
-        host_path_key = volumes_mapping[i]
-        container_path_key = volumes_mapping[i+1]
-        host_path = output_entry.get(host_path_key)
-        container_path = output_entry.get(container_path_key)
+        host_path = volumes_mapping[i]
+        container_path = volumes_mapping[i+1]
         volume_strings.append(f"-v {host_path}:{container_path}")
     return " ".join(volume_strings)
